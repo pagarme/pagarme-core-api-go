a4c6f37926824e761ed529b45fbc6baf50fba09a
Unknown <samuel.leite@mundipagg.com>
2022-03-04 14:00:53 +0000

    5.6.0 -  Add PixPayer in GetPixTransactionResponse

--- src/pagarmecoreapi_lib/pagarmecoreapi_pkg/Client.go
+++ src/pagarmecoreapi_lib/pagarmecoreapi_pkg/Client.go
@@ -10,12 +10,12 @@ import(
 	"pagarmecoreapi_lib/configuration_pkg"
 	"pagarmecoreapi_lib/plans_pkg"
 	"pagarmecoreapi_lib/subscriptions_pkg"
-	"pagarmecoreapi_lib/invoices_pkg"
 	"pagarmecoreapi_lib/orders_pkg"
+	"pagarmecoreapi_lib/invoices_pkg"
 	"pagarmecoreapi_lib/customers_pkg"
-	"pagarmecoreapi_lib/recipients_pkg"
 	"pagarmecoreapi_lib/charges_pkg"
 	"pagarmecoreapi_lib/transfers_pkg"
+	"pagarmecoreapi_lib/recipients_pkg"
 	"pagarmecoreapi_lib/tokens_pkg"
 	"pagarmecoreapi_lib/transactions_pkg"
 )
@@ -25,12 +25,12 @@ import(
 type PAGARMECOREAPI_IMPL struct {
      plans plans_pkg.PLANS
      subscriptions subscriptions_pkg.SUBSCRIPTIONS
-     invoices invoices_pkg.INVOICES
      orders orders_pkg.ORDERS
+     invoices invoices_pkg.INVOICES
      customers customers_pkg.CUSTOMERS
-     recipients recipients_pkg.RECIPIENTS
      charges charges_pkg.CHARGES
      transfers transfers_pkg.TRANSFERS
+     recipients recipients_pkg.RECIPIENTS
      tokens tokens_pkg.TOKENS
      transactions transactions_pkg.TRANSACTIONS
      config  configuration_pkg.CONFIGURATION
@@ -64,16 +64,6 @@ func (me *PAGARMECOREAPI_IMPL) Subscriptions() subscriptions_pkg.SUBSCRIPTIONS {
     return me.subscriptions
 }
 /**
-     * Access to Invoices controller
-     * @return Returns the Invoices() instance
-*/
-func (me *PAGARMECOREAPI_IMPL) Invoices() invoices_pkg.INVOICES {
-    if(me.invoices) == nil {
-        me.invoices = invoices_pkg.NewINVOICES(me.config)
-    }
-    return me.invoices
-}
-/**
      * Access to Orders controller
      * @return Returns the Orders() instance
 */
@@ -84,6 +74,16 @@ func (me *PAGARMECOREAPI_IMPL) Orders() orders_pkg.ORDERS {
     return me.orders
 }
 /**
+     * Access to Invoices controller
+     * @return Returns the Invoices() instance
+*/
+func (me *PAGARMECOREAPI_IMPL) Invoices() invoices_pkg.INVOICES {
+    if(me.invoices) == nil {
+        me.invoices = invoices_pkg.NewINVOICES(me.config)
+    }
+    return me.invoices
+}
+/**
      * Access to Customers controller
      * @return Returns the Customers() instance
 */
@@ -92,16 +92,6 @@ func (me *PAGARMECOREAPI_IMPL) Customers() customers_pkg.CUSTOMERS {
         me.customers = customers_pkg.NewCUSTOMERS(me.config)
     }
     return me.customers
-}
-/**
-     * Access to Recipients controller
-     * @return Returns the Recipients() instance
-*/
-func (me *PAGARMECOREAPI_IMPL) Recipients() recipients_pkg.RECIPIENTS {
-    if(me.recipients) == nil {
-        me.recipients = recipients_pkg.NewRECIPIENTS(me.config)
-    }
-    return me.recipients
 }
 /**
      * Access to Charges controller
@@ -122,6 +112,16 @@ func (me *PAGARMECOREAPI_IMPL) Transfers() transfers_pkg.TRANSFERS {
         me.transfers = transfers_pkg.NewTRANSFERS(me.config)
     }
     return me.transfers
+}
+/**
+     * Access to Recipients controller
+     * @return Returns the Recipients() instance
+*/
+func (me *PAGARMECOREAPI_IMPL) Recipients() recipients_pkg.RECIPIENTS {
+    if(me.recipients) == nil {
+        me.recipients = recipients_pkg.NewRECIPIENTS(me.config)
+    }
+    return me.recipients
 }
 /**
      * Access to Tokens controller
